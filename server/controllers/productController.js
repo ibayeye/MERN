4dc6b60a0b05e7079c94e537310a9f5577b594f2
ibayeye <169161293+ibayeye@users.noreply.github.com>
2024-09-14 18:52:01 +0700

    up
    Filter Produck and Pagination

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -11,7 +11,30 @@ export const createProduct = asyncHandler(async (req, res) => {
 })
 
 export const allProduct = asyncHandler(async (req, res) => {
-    const data = await Product.find();
+
+    const queryObj = { ...req.query }
+
+    const excludeField = ["page", "limit"]
+    excludeField.forEach((element) => delete queryObj[element])
+
+    let query = Product.find(queryObj)
+
+    // Pagination
+    const page = req.query.page * 1 || 1
+    const limitData = req.query.limit * 1 || 30
+    const skipData = (page - 1) * limitData
+
+    query = query.skip(skipData).limit(limitData)
+
+    if (req.query.page) {
+        const numProduct = await Product.countDocuments()
+        if (skipData >= numProduct) {
+            res.status(404)
+            throw new Error("Halaman tidak ditemukan")
+        }
+    }
+
+    const data = await query
 
     res.status(200).json({
         message: "Berhasil menampilkan semua produk",
@@ -40,19 +63,19 @@ export const updateProduct = asyncHandler(async (req, res) => {
 
     const updateProduct = await Product.findByIdAndUpdate(paramsId,
         req.body, {
-            runValidators: false,
-            new: true
-        })
+        runValidators: false,
+        new: true
+    })
 
-        res.status(201).json({
-            message: "Update produk berhasil",
-            data: updateProduct
-        })
+    res.status(201).json({
+        message: "Update produk berhasil",
+        data: updateProduct
+    })
 })
 
 export const deleteProduct = asyncHandler(async (req, res) => {
     const paramsId = req.params.id
-    
+
     const deleteProduct = await Product.findByIdAndDelete(paramsId)
 
     // if (!deleteProduct) {
@@ -69,9 +92,9 @@ export const deleteProduct = asyncHandler(async (req, res) => {
 export const fileUpload = asyncHandler(async (req, res) => {
     const file = req.file;
 
-    if(!file) {
+    if (!file) {
         res.status(400)
-        throw new Error ("Tidak ada file yang diupload")
+        throw new Error("Tidak ada file yang diupload")
     }
 
     const imageFileName = file.filename;
