bc0bd2beb4eb6a6a19079930898bd7403e40b7c5
ibayeye <169161293+ibayeye@users.noreply.github.com>
2024-09-15 14:41:47 +0700

    up
    CREATE orderController

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,8 +1,51 @@
 import asyncHandler from "../middleware/asyncHandler.js"
+import Order from "../models/orderModel.js"
+import Product from "../models/productModel.js"
 
 export const createOrder = asyncHandler(async (req, res) => {
 
+    const {email, firstName, lastName, phone, cartItem} = req.body
+
+    if(!cartItem || cartItem.length < 0) {
+        res.status(400)
+        throw new Error ("Keranjang Masih Kosong")
+    }
+
+    let orderItem = []
+    let total = 0
+
+    for(const cart of cartItem){
+        const productData = await Product.findOne({ _id: cart.product })
+        if (!productData) {
+            res.status(404)
+            throw new Error ("id product tidak ditemukan")
+        }
+        const { name, price, _id } = productData
+        const singleProduct = {
+            quantity: cart.quantity,
+            name,
+            price,
+            product: _id
+        }
+        orderItem = [...orderItem, singleProduct]
+
+        total += cart.quantity * price
+    }
+
+    const order = await Order.create({
+        itemsDetails: orderItem,
+        total,
+        firstName,
+        lastName,
+        email,
+        phone,
+        user : req.user.id
+    })
+
+
     return res.status(201).json({
+        order,
+        total,
         message: "Berhasil order produk",
     })
 })
