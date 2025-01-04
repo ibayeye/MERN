4d54c648db37c447b58289c850ef6da6912d5290
ibayeye <169161293+ibayeye@users.noreply.github.com>
2025-01-04 16:10:30 +0700

    up

--- frontend/src/layout/PublicLayout.jsx
+++ frontend/src/layout/PublicLayout.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import {Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
 
 const PublicLayout = () => {
   return (
     <>
       <div>
-        <header>Header</header>
-        <Outlet/>
-        <footer>Footer</footer>
+        <Header />
+        <Nav/>
+        <main className="mx-auto max-w-6xl px-8 py-20 min-h-[80vh]">
+          <Outlet />
+        </main>
+        <Footer/>
       </div>
     </>
   );
