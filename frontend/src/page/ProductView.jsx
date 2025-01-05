b009c555c806ea2d6c8c79164210b99902b4a963
ibayeye <169161293+ibayeye@users.noreply.github.com>
2025-01-05 14:36:38 +0700

    up
    Buat Fitur Filter Dengan ReactJS

--- frontend/src/page/ProductView.jsx
+++ frontend/src/page/ProductView.jsx
@@ -5,12 +5,16 @@ import Filter from "../components/Filter";
 import CardProduct from "../components/CardProduct";
 
 export const loader = async ({ request }) => {
-  const { data } = await customAPI.get("/product");
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  const { data } = await customAPI.get("/product", { params: params });
 
   console.log(request);
+  console.log(params);
 
   const products = data.data;
-  return { products };
+  return { products, params };
 };
 const ProductView = () => {
   const { products } = useLoaderData();
@@ -19,9 +23,11 @@ const ProductView = () => {
     <>
       <Filter />
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 mt-5">
-        {products.map((item) => (
-          <CardProduct key={item._id} item={item} />
-        ))}
+        {!products.length ? (
+          <h1 className="text-3xl font-bold mt-5">Produk tidak ditemukan</h1>
+        ) : (
+          products.map((item) => <CardProduct key={item._id} item={item} />)
+        )}
       </div>
     </>
   );
