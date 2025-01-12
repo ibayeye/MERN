6a6652b2e251d68288139216087f8058ff895752
ibayeye <169161293+ibayeye@users.noreply.github.com>
2025-01-12 10:49:17 +0700

    redeploy payment

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -119,7 +119,7 @@ export const currentOrder = asyncHandler(async (req, res) => {
 })
 
 export const callbackPayment = asyncHandler(async (req, res) => {
-    const statusResponse = apiClient.transaction.notification(req.body)
+    const statusResponse = await snap.transaction.notification(req.body)
 
     let orderId = statusResponse.order_id;
     let transactionStatus = statusResponse.transaction_status;
